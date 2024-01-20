cecb86d050beed6dbfd273c3c7643cd51ac6503b
lemstudent <lemstudent@LAPTOP-4VE2S21N>
2024-01-20 14:24:16 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Net;
+
+//  Author:      Demetrius Richards
+//  Course:      COMP-003A-L01
+//  Purpose:     Code For Assignment 1
+//
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Myself");
+            Console.WriteLine("1.Favorite colors, Black")
+            Console.WriteLine("2. Green")
+            Console.WriteLine("3. Red")
+            Console.WriteLine("4. Gray")
+            Console.WriteLine("5. White")
+            Console.WriteLine("6. Crimson")
+            Console.WriteLine("7. Onyx")
+            Console.WriteLine("8. Favorite Foods, Wings")
+            Console.WriteLine("9. Chinese food")
+            Console.WriteLine("10. Burgers")
+            Console.WriteLine("11. Potstickers")
+            Console.WriteLine("12. Hobbies, Video games")
+            Console.WriteLine("13. Beat making")
+            Console.WriteLine("14. Fashion")
+            Console.WriteLine("15. Listening to Music")
+            Console.WriteLine("16. Favorite Artists, Playboi Carti")
+            Console.WriteLine("17. Ken Carson")
+            Console.WriteLine("18. Destroy Lonely")
+            Console.WriteLine("19. Homoxide Gang")
+            Console.WriteLine("20. Travis Scott")
+            Console.WriteLine("21. Lil Uzi")
+            Console.WriteLine("22. Benji Blue Bills")
+            Console.WriteLine("23. Played Soccer")
+            Console.WriteLine("24. Did Boxing")
+            Console.WriteLine("25. Won Spelling bee in 4th grade")
+            Console.WriteLine("26. Won Hanford soccer championship in 2013")
+            Console.WriteLine("27. Own 5 different consoles")
+            Console.WriteLine("28. Worked 2 jobs so far")
+            Console.WriteLine("29. Baskin Robbins")
+            Console.WriteLine("30. Carls Jr")
+            Console.WriteLine("31. This will be my 3rd semester")
+            Console.WriteLine("32. Born in a military base")
+            Console.WriteLine("33. Played soccer for 7 years ")
+            Console.WriteLine("34. Graduated from Sierra Pacific High")
+            Console.WriteLine("35. Favorite Subject is Science")
+            Console.WriteLine("36. Favorite Clothing Lines, Works Of Madness")
+            Console.WriteLine("37. Timberland")
+            Console.WriteLine("38. Jordan")
+            Console.WriteLine("39. Rockstar Original")
+            Console.WriteLine("40. Guapi")
+            Console.WriteLine("41. Demontshop")
+            Console.WriteLine("42. MADMANN INC")
+            Console.WriteLine("43. Made In Idle Time")
+            Console.WriteLine("44. UNINTENDED")
+            Console.WriteLine("45. South Of Heaven")
+            Console.WriteLine("46. Favorite Producers/ Beatmakers, Art Dealer")
+            Console.WriteLine("47. Outta Town")
+            Console.WriteLine("48. Mexiko Dro")
+            Console.WriteLine("49. F1lthy")
+            Console.WriteLine("50. BNYX")
+            Console.WriteLine("51. Starboy")
+            Console.WriteLine("52. Oji Volta")
+            Console.WriteLine("53. Kanye West")
+            Console.WriteLine("54. Pierre Bourne")
+            Console.WriteLine("55. Working On Dying")
+            Console.WriteLine("56. Clams Casino")
+            Console.WriteLine("57. Prod. Mayday")
+            Console.WriteLine("58. Favorite Video Games, Elden Ring")
+            Console.WriteLine("59. PVZ")
+            Console.WriteLine("60. Modern Warfare 3 OG")
+            Console.WriteLine("61. Fortnite ")
+            Console.WriteLine("62. Rainbow Six Siege")
+            Console.WriteLine("63. Lies Of P")
+            Console.WriteLine("64. Terraria")
+            Console.WriteLine("65. Minecraft")
+            Console.WriteLine("66. Doom")
+            Console.WriteLine("67. Mass Effect")
+            Console.WriteLine("68. Gears of War")
+            Console.WriteLine("69. Rust")
+            Console.WriteLine("70. Fallout 4")
+            Console.WriteLine("71. Darksiders")
+            Console.WriteLine("72. Sonic Unleashed")
+            Console.WriteLine("73. Left 4 Dead")
+            Console.WriteLine("74. Cuphead")
+            Console.WriteLine("75. Stardew Valley")
+            Console.WriteLine("76.Been to Hawaii")
+            Console.WriteLine("77. Been to Florida")
+            Console.WriteLine("78. Been to Mexico")
+            Console.WriteLine("79. Been To Vegas")
+            Console.WriteLine("80. Been to Texas")
+            Console.WriteLine("81. Been to Arizona")
+            Console.WriteLine("82. Can Play piano/somewhat")
+            Console.WriteLine("83. Favorite instruments, Guitar")
+            Console.WriteLine("84. Piano")
+            Console.WriteLine("85. Synth")
+            Console.WriteLine("86. 808")
+            Console.WriteLine("87. Flute")
+            Console.WriteLine("88. Own an Xbox")
+            Console.WriteLine("89. Own an ps5")
+            Console.WriteLine("90. Own a nintendo switch")
+            Console.WriteLine("91. Drive an Expedition")
+            Console.WriteLine("92. Got drivers licence one month after 16th birthday")
+            Console.WriteLine("93. 19 years of age")
+            Console.WriteLine("94. 6'2 in height")
+            Console.WriteLine("95. Mixed/ Black and Hispanic")
+            Console.WriteLine("96. Was going to join military but changed mind")
+            Console.WriteLine("97. Cybersecurity Major")
+            Console.WriteLine("98. Birthday Is October 13th, 2004")
+            Console.WriteLine("99. Been to two concerts")
+            Console.WriteLine("100. Afraid of Heights")
+
+
+
+
+
+
+
+
+
+
+
+
